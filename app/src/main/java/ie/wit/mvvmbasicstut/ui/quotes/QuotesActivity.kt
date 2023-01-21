3c54d83e485de991364c54d6ad847b8285c85547
ahsa91 <ahsa91@gmail.com>
2023-01-21 21:16:00 +0000

    organizing/adding data class/dao class

--- app/src/main/java/ie/wit/mvvmbasicstut/ui/quotes/QuotesActivity.kt
+++ app/src/main/java/ie/wit/mvvmbasicstut/ui/quotes/QuotesActivity.kt
@@ -1,11 +1,12 @@
-package ie.wit.mvvmbasicstut
+package ie.wit.mvvmbasicstut.ui.quotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ie.wit.mvvmbasicstut.R
 
-class MainActivity : AppCompatActivity() {
+class QuotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_quotes)
     }
 }
